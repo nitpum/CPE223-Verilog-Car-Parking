b9725d6408216a018c12e94f95e804b8975af86c
nitpum <nitpum@outlook.com>
2018-12-09 17:01:02 +0700

    Update dot display

--- Lab8.srcs/sources_1/new/display.v
+++ Lab8.srcs/sources_1/new/display.v
@@ -22,12 +22,14 @@
 
 module display(
     input clk, 
+    input [3:0] dot,
     input [6:0] seg1,
     input [6:0] seg2,
     input [6:0] seg3,
     input [6:0] seg4,
     output reg [6:0] seg,
-    output reg [3:0] ano
+    output reg [3:0] ano,
+    output reg dp
     );
     wire [1:0] led_actived_counter;
     reg [19:0] refresh_counter;
@@ -40,18 +42,22 @@ module display(
             2'b00 : begin // Deactive LED2, LED3, LED4
                     ano = 4'b0111;
                     seg = seg1;
+                    dp <= dot[3];
                 end
             2'b01 : begin // Deactive LED1, LED3, LED4
                     ano = 4'b1011;
                     seg = seg2;
+                    dp <= dot[2];
                 end
             2'b10 : begin
                     ano = 4'b1101; // Deactive LED1, LED2, LED4
                     seg = seg3;
+                    dp <= dot[1];
                 end
             2'b11 : begin
                     ano = 4'b1110; // Deactive LED1, LED2, LED3
                     seg = seg4;
+                    dp <= dot[0];
                 end
         endcase
     end
--- Lab8.srcs/sources_1/new/top_module.v
+++ Lab8.srcs/sources_1/new/top_module.v
@@ -24,7 +24,8 @@ module top_module(
     input clk,
     input [4:0] sw,    
     output [6:0] seg,
-    output [3:0] an
+    output [3:0] an,
+    output dp
     );
     wire clk_sec;
     reg [6:0] seg1;
@@ -36,6 +37,7 @@ module top_module(
     wire [6:0] timer3;
     wire [6:0] timer4;
     wire [10:0] timer;
+    reg [3:0] dot;
     divide_sec(clk, clk_sec);
     time_counter(clk_sec, 0, timer);
     bcdto7seg(timer / 60 / 10, timer1);
@@ -43,24 +45,29 @@ module top_module(
     bcdto7seg(timer % 60 / 10, timer3);
     bcdto7seg(timer % 10, timer4);
     always @ (sw) begin
-        if (sw == 0) begin
-            seg1 = timer1;
-            seg2 = timer2;
-            seg3 = timer3;
-            seg4 = timer4;
+        case (sw)
+            default: begin
+                seg1 = timer1;
+                seg2 = timer2;
+                seg3 = timer3;
+                seg4 = timer4;
+                dot = 4'b1011;    
             end
-        else if (sw == 1) begin
-            seg1 = 7'b1111001;
-            seg2 = 7'b0101011;
-            seg3 = 7'b1111111;
-            seg4 = 7'b1111001;
+            1: begin
+                seg1 = 7'b1111001;
+                seg2 = 7'b0101011;
+                seg3 = 7'b1111111;
+                seg4 = 7'b1111001;
+                dot = 4'b1111;    
             end
-        else if (sw == 2) begin
-            seg1 = 7'b1000000;
-            seg2 = 7'b1100011;
-            seg3 = 7'b0000111;
-            seg4 = 7'b1111001;
+            2: begin
+                seg1 = 7'b1000000;
+                seg2 = 7'b1100011;
+                seg3 = 7'b0000111;
+                seg4 = 7'b1111001;
+                dot = 4'b1111; 
             end
+        endcase
     end
-    display(clk, seg1, seg2, seg3, seg4, seg, an);
+    display(clk, dot, seg1, seg2, seg3, seg4, seg, an, dp);
 endmodule
